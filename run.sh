9fab4cdf7421ec8893f4f2660c50636995538553
Lukas <lukas.korous@gmail.com>
2013-01-11 17:16:05 -0700

    Add 02-adapt to memory leaks.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 cmake .
+echo "This test suite will fail if you do not have Hermes, or Valgrind, etc.."
 echo "CMake generation done."
 echo "Run memory-leaks tests? [y/n]"
 read ans
@@ -9,6 +10,10 @@ if [ "$ans" = "y" ]; then
   make
   valgrind --leak-check=full --log-file=../01-simple-ValgrindLogfile ./01-simple 3 3
   echo "Valgrind output '01-simple-ValgrindLogfile' available in memory-leaks/"
+  cd ../02-adapt
+  make
+  valgrind --leak-check=full --log-file=../02-adapt-ValgrindLogfile ./02-adapt
+  echo "Valgrind output '02-adapt-ValgrindLogfile' available in memory-leaks/"
   echo "Memory leaks tests - Done."
 fi
 echo "Run visualization tests? [y/n]"
