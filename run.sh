9d9135fdfd574d8239f78359faff4b98271a1215
Lukas <lukas.korous@gmail.com>
2013-01-12 20:36:46 -0700

    Progress.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 cmake .
 echo "This test suite will fail if you do not have Hermes, or Valgrind, etc.."
 echo "CMake generation done."
-echo "Run memory-leaks tests? [y/n]"
+echo "Run memory-leaks tests? (Will take some time) [y/n]"
 read ans
 if [ "$ans" = "y" ]; then
 	echo "Processing memory leaks tests..."
@@ -15,6 +15,7 @@ if [ "$ans" = "y" ]; then
   valgrind --leak-check=full --log-file=../02-adapt-ValgrindLogfile ./02-adapt
   echo "Valgrind output '02-adapt-ValgrindLogfile' available in memory-leaks/"
   echo "Memory leaks tests - Done."
+  cd ../..
 fi
 echo "Run visualization tests? [y/n]"
 read ans
@@ -26,5 +27,26 @@ if [ "$ans" = "y" ]; then
   ./01-all
   echo "Outputs saved in visualization/views/*.bmp"
   echo "Visualization tests - Done."
+  cd ../../..
 fi
-echo "Quitting"
+echo "Run inner-funcionality tests? [y/n]"
+read ans
+if [ "$ans" = "y" ]; then
+	echo "Processing internal tests..."
+  cd inner-functionality
+  make -j4
+  ctest -j4
+  echo "Internal-functionality tests - Done."
+  cd ..
+fi
+echo "Run calculations tests? [y/n]"
+read ans
+if [ "$ans" = "y" ]; then
+	echo "Processing calculations tests..."
+  cd calculations
+  make -j4
+  ctest -j4
+  echo "Calculations tests - Done."
+
+fi
+echo "Quitting..."
