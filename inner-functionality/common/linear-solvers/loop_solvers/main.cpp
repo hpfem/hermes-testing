1df7ad8f64a98766c62fb60b4bf20b9a7b600748
Lukas Korous <lukas.korous@gmail.com>
2013-09-26 09:02:35 +0200

    Preparation work on tests of loop solvers.

---
+++ inner-functionality/common/linear-solvers/loop_solvers/main.cpp
@@ -0,0 +1,426 @@
+#include "../../../../testing-core/testing-core.h"
+
+using namespace Hermes::Algebra::DenseMatrixOperations;
+using namespace Hermes::Solvers;
+
+// Test of linear solvers.
+// Read matrix and RHS from a file.
+
+// Max row length in input file.
+#define MAX_ROW_LEN  1024
+
+class MatrixEntry
+{
+public:
+  MatrixEntry() { }
+  MatrixEntry(int m, int n, double value) {
+    this->m = m;
+    this->n = n;
+    this->value = value;
+  }
+
+  int m, n;
+  double  value;
+};
+
+void show_mat(const char *msg, std::map<unsigned int, MatrixEntry> mp)
+{
+  std::map<unsigned int, MatrixEntry>::iterator itr;
+
+  std::cout << msg << std::endl;
+
+  for(itr = mp.begin(); itr != mp.end(); ++itr)
+    std::cout << " " << (int) itr->first << ": " <<
+    (int) itr->second.m << " " <<
+    (int) itr->second.n << " " <<
+    (double) itr->second.value <<
+    std::endl;
+
+  std::cout << std::endl;
+}
+
+void show_rhs(const char *msg, std::map<unsigned int, double > mp) {
+  std::map<unsigned int, double >::iterator itr;
+
+  std::cout << msg << std::endl;
+
+  for(itr = mp.begin(); itr != mp.end(); ++itr)
+    std::cout << " " << (int) itr->first << ": " << (double) itr->second << std::endl;
+
+  std::cout << std::endl;
+}
+
+bool testPrint(bool value, const char *msg, bool correct) {
+  if(value == correct) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+bool read_n_nums(char *row, int n, double values[]) {
+  int i = 0;
+  char delims[] = " \t\n\r";
+  char *token = strtok(row, delims);
+  while (token != NULL && i < n) {
+    double entry_buffer;
+    sscanf(token, "%lf", &entry_buffer);
+    values[i++] = entry_buffer;
+
+    token = strtok(NULL, delims);
+  }
+
+  return (i == n);
+}
+
+int read_matrix_and_rhs(char *file_name, int &n, int &nnz, std::map<unsigned int, MatrixEntry>& mat, std::map<unsigned int, double>& rhs)
+{
+  FILE *file = fopen(file_name, "r");
+  if(file == NULL)
+    return -1;
+
+  enum EState {
+    STATE_N,
+    STATE_MATRIX,
+    STATE_RHS,
+    STATE_NNZ
+  }
+  state = STATE_N;
+
+  double buffer[4];
+  char row[MAX_ROW_LEN];
+  while (fgets(row, MAX_ROW_LEN, file) != NULL) {
+    switch (state) {
+    case STATE_N:
+      if(read_n_nums(row, 1, buffer))
+      {
+        n = (int) buffer[0];
+        state = STATE_NNZ;
+      }
+      break;
+
+    case STATE_NNZ:
+      if(read_n_nums(row, 1, buffer))
+        nnz = (int) buffer[0];
+
+      state = STATE_MATRIX;
+      break;
+
+    case STATE_MATRIX:
+      if(read_n_nums(row, 3, buffer))
+        mat[mat.size()] = (MatrixEntry ((int) buffer[0], (int) buffer[1], buffer[2]));
+      else
+        state = STATE_RHS;
+      break; //case STATE_MATRIX break.
+
+    case STATE_RHS:
+      { // if cplx_2_real is false.
+        if(read_n_nums(row, 2, buffer))
+          rhs[(int) buffer[0]] = (double) buffer[1];
+      }
+      break;
+    }
+  }
+
+  fclose(file);
+
+  return 0;
+}
+
+void build_matrix(int n, std::map<unsigned int, MatrixEntry> &ar_mat, std::map<unsigned int, double > &ar_rhs,
+                  SparseMatrix<double> *mat, Vector<double> *rhs)
+{
+  mat->prealloc(n);
+  for (std::map<unsigned int, MatrixEntry>::iterator it = ar_mat.begin(); it != ar_mat.end(); it++) {
+    MatrixEntry &me = it->second;
+    mat->pre_add_ij(me.m, me.n);
+  }
+
+  mat->alloc();
+  for (std::map<unsigned int, MatrixEntry>::iterator it = ar_mat.begin(); it != ar_mat.end(); it++) {
+    MatrixEntry &me = it->second;
+    mat->add(me.m, me.n, me.value);
+  }
+  mat->finish();
+
+  rhs->alloc(n);
+  for (std::map<unsigned int, double >::iterator it = ar_rhs.begin(); it != ar_rhs.end(); it++) {
+    rhs->add(it->first, it->second);
+  }
+  rhs->finish();
+}
+
+void build_matrix_block(int n, std::map<unsigned int, MatrixEntry> &ar_mat, std::map<unsigned int, double > &ar_rhs,
+                        SparseMatrix<double> *matrix, Vector<double> *rhs) {
+                          matrix->prealloc(n);
+                          for (int i = 0; i < n; i++)
+                            for (int j = 0; j < n; j++)
+                              matrix->pre_add_ij(i, j);
+
+                          matrix->alloc();
+                          double  **mat = new_matrix<double>(n, n);
+                          int *cols = new int[n];
+                          int *rows = new int[n];
+                          for (int i = 0; i < n; i++) {
+                            cols[i] = i;
+                            rows[i] = i;
+                          }
+                          for (std::map<unsigned int, MatrixEntry>::iterator it = ar_mat.begin(); it != ar_mat.end(); it++) {
+                            MatrixEntry &me = it->second;
+                            mat[me.m][me.n] = me.value;
+                          }
+                          matrix->add(n, n, mat, rows, cols);
+                          matrix->finish();
+
+                          rhs->alloc(n);
+                          double  *rs = new double[n];
+                          for (std::map<unsigned int, double >::iterator it = ar_rhs.begin(); it != ar_rhs.end(); it++) {
+                            rs[it->first] = it->second;
+                          }
+                          unsigned int *u_rows = new unsigned int[n];
+                          for (int i = 0; i < n; i++)
+                            u_rows[i] = rows[i] >= 0 ? rows[i] : 0;
+                          rhs->add(n, u_rows, rs);
+                          rhs->finish();
+}
+
+// Test code.
+void solve(LinearMatrixSolver<double> &solver, int n)
+{
+  solver.solve();
+}
+
+int main(int argc, char *argv[]) {
+  int ret = 0;
+
+  int n;
+  int nnz;
+
+  std::map<unsigned int, MatrixEntry> ar_mat;
+  std::map<unsigned int, double > ar_rhs;
+
+  double* sln = NULL;
+  switch(atoi(argv[2]))
+  {
+  case 1:
+    if(read_matrix_and_rhs((char*)"in/linsys-1", n, nnz, ar_mat, ar_rhs) != 0)
+      throw Hermes::Exceptions::Exception("Failed to read the matrix and rhs.");
+    break;
+  case 2:
+    if(read_matrix_and_rhs((char*)"in/linsys-2", n, nnz, ar_mat, ar_rhs) != 0)
+      throw Hermes::Exceptions::Exception("Failed to read the matrix and rhs.");
+    break;
+  case 3:
+    if(read_matrix_and_rhs((char*)"in/linsys-3", n, nnz, ar_mat, ar_rhs) != 0)
+      throw Hermes::Exceptions::Exception("Failed to read the matrix and rhs.");
+    break;
+  }
+
+  if(strcasecmp(argv[1], "petsc") == 0) {
+#ifdef WITH_PETSC
+    PetscMatrix<double> mat;
+    PetscVector<double> rhs;
+    build_matrix(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    PetscLinearMatrixSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "petsc-block") == 0) {
+#ifdef WITH_PETSC
+    PetscMatrix<double> mat;
+    PetscVector<double> rhs;
+    build_matrix_block(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    PetscLinearMatrixSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "umfpack") == 0) {
+#ifdef WITH_UMFPACK
+    CSCMatrix<double> mat;
+    SimpleVector<double> rhs;
+    build_matrix(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    UMFPackLinearMatrixSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "umfpack-block") == 0) {
+#ifdef WITH_UMFPACK
+    CSCMatrix<double> mat;
+    SimpleVector<double> rhs;
+    build_matrix_block(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    UMFPackLinearMatrixSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "paralution") == 0) {
+#ifdef WITH_PARALUTION
+    ParalutionMatrix<double> mat;
+    ParalutionVector<double> rhs;
+    build_matrix(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    Hermes::Solvers::IterativeParalutionLinearMatrixSolver<double> solver(&mat, &rhs);
+    if(atoi(argv[2]) != 1)
+      solver.set_solver_type(BiCGStab);
+    solver.set_precond(new ParalutionPrecond<double>(ILU));
+    // Tested as of 13th August 2013.
+    solver.set_max_iters(atoi(argv[2]) == 1 ? 1 : atoi(argv[2]) == 2 ? 2 : 5);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "paralution-block") == 0) {
+#ifdef WITH_PARALUTION
+    ParalutionMatrix<double> mat;
+    ParalutionVector<double> rhs;
+    build_matrix_block(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    Hermes::Solvers::IterativeParalutionLinearMatrixSolver<double> solver(&mat, &rhs);
+    solver.set_precond(new ParalutionPrecond<double>(ILU));
+    solver.set_max_iters(10000);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "superlu") == 0) {
+#ifdef WITH_SUPERLU
+    CSCMatrix<double> mat;
+    SimpleVector<double> rhs;
+    build_matrix(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    Hermes::Solvers::SuperLUSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "superlu-block") == 0) {
+#ifdef WITH_SUPERLU
+    CSCMatrix<double> mat;
+    SimpleVector<double> rhs;
+    build_matrix_block(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    Hermes::Solvers::SuperLUSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "aztecoo") == 0) {
+#ifdef WITH_TRILINOS
+    EpetraMatrix<double> mat;
+    EpetraVector<double> rhs;
+    build_matrix(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    AztecOOSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "aztecoo-block") == 0) {
+#ifdef WITH_TRILINOS
+    EpetraMatrix<double> mat;
+    EpetraVector<double> rhs;
+    build_matrix_block(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    AztecOOSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "amesos") == 0) {
+#ifdef WITH_TRILINOS
+    EpetraMatrix<double> mat;
+    EpetraVector<double> rhs;
+    build_matrix(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    if(AmesosSolver<double>::is_available("Klu")) {
+      AmesosSolver<double> solver("Klu", &mat, &rhs);
+      solve(solver, n);
+      sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+    }
+#endif
+  }
+  else if(strcasecmp(argv[1], "amesos-block") == 0) {
+#ifdef WITH_TRILINOS
+    EpetraMatrix<double> mat;
+    EpetraVector<double> rhs;
+    build_matrix_block(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    if(AmesosSolver<double>::is_available("Klu")) {
+      AmesosSolver<double> solver("Klu", &mat, &rhs);
+      solve(solver, n);
+      sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+    }
+#endif
+  }
+  else if(strcasecmp(argv[1], "mumps") == 0) {
+#ifdef WITH_MUMPS
+    MumpsMatrix<double> mat;
+    SimpleVector<double> rhs;
+    build_matrix(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    MumpsSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+  else if(strcasecmp(argv[1], "mumps-block") == 0) {
+#ifdef WITH_MUMPS
+    MumpsMatrix<double> mat;
+    SimpleVector<double> rhs;
+    build_matrix_block(n, ar_mat, ar_rhs, &mat, &rhs);
+
+    MumpsSolver<double> solver(&mat, &rhs);
+    solve(solver, n);
+    sln = new double[mat.get_size()]; memcpy(sln, solver.get_sln_vector(), mat.get_size() * sizeof(double));
+#endif
+  }
+
+  bool success = true;
+  if(sln)
+  {
+    switch(atoi(argv[2]))
+    {
+    case 1:
+      success = Testing::test_value(sln[0], 4, "sln[0]", 1E-6) && success;
+      success = Testing::test_value(sln[1], 2, "sln[1]", 1E-6) && success;
+      success = Testing::test_value(sln[2], 3, "sln[2]", 1E-6) && success;
+      break;
+    case 2:
+      success = Testing::test_value(sln[0], 2, "sln[0]", 1E-6) && success;
+      success = Testing::test_value(sln[1], 3, "sln[1]", 1E-6) && success;
+      success = Testing::test_value(sln[2], 1, "sln[2]", 1E-6) && success;
+      success = Testing::test_value(sln[3], -3, "sln[3]", 1E-6) && success;
+      success = Testing::test_value(sln[4], -1, "sln[4]", 1E-6) && success;
+      break;
+    case 3:
+      success = Testing::test_value(sln[0], 1, "sln[0]", 1E-6) && success;
+      success = Testing::test_value(sln[1], 2, "sln[1]", 1E-6) && success;
+      success = Testing::test_value(sln[2], 3, "sln[2]", 1E-6) && success;
+      success = Testing::test_value(sln[3], 4, "sln[3]", 1E-6) && success;
+      success = Testing::test_value(sln[4], 5, "sln[4]", 1E-6) && success;
+      break;
+    }
+
+    if(success)
+    {
+
+      printf("Success!\n");
+      return 0;
+    }
+    else
+    {
+      printf("Failure!\n");
+      return -1;
+    } 
+  }
+  else
+    return 0;
+}
---
+++ inner-functionality/common/linear-solvers/loop_solvers/stationary-nonlinear/main.cpp
@@ -0,0 +1,149 @@
+#include "definitions.h"
+#include "../../../../../testing-core/testing-core.h"
+
+// Initial polynomial degree.
+const int P_INIT = 1;                             
+// Stopping criterion for the Newton's method.
+const double NEWTON_TOL = 1e-4;                   
+// Maximum allowed number of Newton iterations.
+const int NEWTON_MAX_ITER = 100;                  
+// Number of initial uniform mesh refinements.
+const int INIT_GLOB_REF_NUM = 2;                  
+// Number of initial refinements towards boundary.
+const int INIT_BDY_REF_NUM = 3;                   
+
+// Problem parameters.
+double heat_src = 1.0;
+double alpha = 7.0;
+
+int main(int argc, char* argv[])
+{
+  HermesCommonApi.set_integral_param_value(matrixSolverType, SOLVER_PARALUTION_ITERATIVE);
+  // Load the mesh.
+  MeshSharedPtr mesh(new Mesh);
+  MeshReaderH2D mloader;
+  mloader.load("square.mesh", mesh);
+
+  // Perform initial mesh refinements.
+  for(int i = 0; i < INIT_GLOB_REF_NUM; i++) mesh->refine_all_elements();
+  mesh->refine_towards_boundary("Bdy", INIT_BDY_REF_NUM);
+
+  // Initialize boundary conditions.
+  CustomEssentialBCNonConst bc_essential("Bdy");
+  EssentialBCs<double> bcs(&bc_essential);
+
+  // Create an H1 space with default shapeset.
+  SpaceSharedPtr<double> space(new H1Space<double>(mesh, &bcs, P_INIT));
+  int ndof = space->get_num_dofs();
+
+  // Initialize the weak formulation
+  CustomNonlinearity lambda(alpha);
+  Hermes2DFunction<double> src(-heat_src);
+  DefaultWeakFormPoisson<double> wf(HERMES_ANY, &lambda, &src);
+  #ifdef SHOW_OUTPUT
+        ScalarView s_view("Solution");
+      #endif
+
+  double* coeff_vec = new double[ndof];
+  MeshFunctionSharedPtr<double> init_sln(new CustomInitialCondition(mesh));
+  OGProjection<double> ogProjection; ogProjection.project_global(space, init_sln, coeff_vec); 
+  MeshFunctionSharedPtr<double> sln(new Solution<double>);
+
+  // Testing is happening here.
+  Hermes::vector<int> numbers_of_nonlinear_iterations;
+  Hermes::vector<int> numbers_of_linear_iterations_in_last_nonlinear_step;
+  // Iterative - zero initial guess.
+  HermesCommonApi.set_integral_param_value(matrixSolverType, SOLVER_PARALUTION_ITERATIVE);
+  {
+    // Initialize Newton solver.
+    NewtonSolver<double> newton(&wf, space);
+    newton.set_tolerance(NEWTON_TOL, ResidualNormAbsolute);
+    newton.set_max_steps_with_reused_jacobian(0);
+
+    IterativeParalutionLinearMatrixSolver<double>* lsolver = dynamic_cast<IterativeParalutionLinearMatrixSolver<double>*>(newton.get_linear_solver());
+    lsolver->set_solver_type(GMRES);
+    //lsolver->set_precond(new ParalutionPrecond<double>(Preconditioners::PreconditionerType::MultiColoredSGS));
+    lsolver->set_tolerance(1e-5);
+
+    // Perform Newton's iteration.
+    newton.solve(coeff_vec);
+    numbers_of_nonlinear_iterations.push_back(newton.get_num_iters());
+    numbers_of_linear_iterations_in_last_nonlinear_step.push_back(lsolver->get_num_iters());
+      
+    // Translate the resulting coefficient vector into a Solution.
+    Solution<double>::vector_to_solution(newton.get_sln_vector(), space, sln);
+    #ifdef SHOW_OUTPUT
+      s_view.show(sln);
+    #endif
+  }
+
+  // Iterative - nonzero initial guess.
+  {
+    // Initialize Newton solver.
+    NewtonSolver<double> newton(&wf, space);
+    newton.set_tolerance(NEWTON_TOL, ResidualNormAbsolute);
+
+    IterativeParalutionLinearMatrixSolver<double>* lsolver = dynamic_cast<IterativeParalutionLinearMatrixSolver<double>*>(newton.get_linear_solver());
+    lsolver->set_solver_type(GMRES);
+
+    // Perform Newton's iteration.
+    newton.solve(sln);
+    numbers_of_nonlinear_iterations.push_back(newton.get_num_iters());
+    numbers_of_linear_iterations_in_last_nonlinear_step.push_back(lsolver->get_num_iters());
+      
+    // Translate the resulting coefficient vector into a Solution.
+    Solution<double>::vector_to_solution(newton.get_sln_vector(), space, sln);
+    #ifdef SHOW_OUTPUT
+      s_view.show(sln);
+    #endif
+  }
+  // AMG - nonzero initial guess.
+  HermesCommonApi.set_integral_param_value(matrixSolverType, SOLVER_PARALUTION_AMG);
+  {
+    // Initialize Newton solver.
+    NewtonSolver<double> newton(&wf, space);
+    newton.set_tolerance(NEWTON_TOL, ResidualNormAbsolute);
+
+    AMGParalutionLinearMatrixSolver<double>* lsolver = dynamic_cast<AMGParalutionLinearMatrixSolver<double>*>(newton.get_linear_solver());
+    lsolver->set_smoother(GMRES, Preconditioners::ILU);
+
+    // Perform Newton's iteration.
+    newton.solve(coeff_vec);
+    numbers_of_nonlinear_iterations.push_back(newton.get_num_iters());
+    numbers_of_linear_iterations_in_last_nonlinear_step.push_back(lsolver->get_num_iters());
+      
+    // Translate the resulting coefficient vector into a Solution.
+    Solution<double>::vector_to_solution(newton.get_sln_vector(), space, sln);
+    #ifdef SHOW_OUTPUT
+      s_view.show(sln);
+    #endif
+  }
+
+  // Iterative - nonzero initial guess.
+  {
+      // Initialize Newton solver.
+      NewtonSolver<double> newton(&wf, space);
+      newton.set_tolerance(NEWTON_TOL, ResidualNormAbsolute);
+
+      AMGParalutionLinearMatrixSolver<double>* lsolver = dynamic_cast<AMGParalutionLinearMatrixSolver<double>*>(newton.get_linear_solver());
+      lsolver->set_smoother(GMRES, Preconditioners::ILU);
+
+      // Perform Newton's iteration.
+      newton.solve(sln);
+      numbers_of_nonlinear_iterations.push_back(newton.get_num_iters());
+      numbers_of_linear_iterations_in_last_nonlinear_step.push_back(lsolver->get_num_iters());
+      
+      // Translate the resulting coefficient vector into a Solution.
+      Solution<double>::vector_to_solution(newton.get_sln_vector(), space, sln);
+      #ifdef SHOW_OUTPUT
+        s_view.show(sln);
+      #endif
+  }
+
+  // Clean up.
+  delete [] coeff_vec;
+
+  printf("Success!\n");
+  return 0;
+}
+
