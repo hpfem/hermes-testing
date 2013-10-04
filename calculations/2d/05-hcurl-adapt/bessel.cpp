0b5cf7c58880837d0c1ff3fba3c4514854892aa2
Lukas Korous <lukas.korous@gmail.com>
2013-10-04 11:10:59 +0200

    Fix a test.

--- calculations/2d/05-hcurl-adapt/bessel.cpp
+++ calculations/2d/05-hcurl-adapt/bessel.cpp
@@ -2349,7 +2349,14 @@ extern double fabs ( double );
 // extern double j0 ( double );
 // extern double j1 ( double );
 extern double sqrt ( double );
-extern double cbrt ( double );
+double cbrt(double x)
+{
+  if(!_isnan(x))
+    return std::pow(x, 1.0 / 3.0);
+  else
+    return x;
+}
+
 extern double exp ( double );
 extern double log ( double );
 extern double sin ( double );
