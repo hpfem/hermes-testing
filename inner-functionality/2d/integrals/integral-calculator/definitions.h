2749520b83cd4f208c67c7a7b8b0b0db8867f320
Lukas Korous <lukas.korous@gmail.com>
2013-10-12 16:22:11 +0200

    Update the integral calculator test.

--- inner-functionality/2d/integrals/integral-calculator/definitions.h
+++ inner-functionality/2d/integrals/integral-calculator/definitions.h
@@ -30,20 +30,18 @@ public:
   /// \param[in] result - preallocated (see number_of_integrals in the constructor) and zeroed array for the results.
   virtual void integral(int n, double* wt, Func<double> **fns, Geom<double> *e, double* result)
   {
-    for (int i = 0; i < n; i++)
-    {
-      result[0] = wt[i] * fns[0]->val[i];
-      result[1] = wt[i] * fns[0]->val[i];
-      result[2] = wt[i] * fns[0]->val[i];
-    }
+    for(int j = 0; j < this->number_of_integrals; j++)
+      for (int i = 0; i < n; i++)
+      {
+        result[j] += wt[i] * fns[0]->val[i];
+      }
   };
 
   /// The integration order calculation.
   virtual void order(Func<Hermes::Ord> **fns, Hermes::Ord* result)
   {
-    result[0] = fns[0]->val[0];
-    result[1] = fns[0]->val[0];
-    result[2] = fns[0]->val[0];
+    for(int j = 0; j < this->number_of_integrals; j++)
+      result[j] = fns[0]->val[0];
   }
 };
 
@@ -58,13 +56,15 @@ public:
   /// \param[in] result - preallocated (see number_of_integrals in the constructor) and zeroed array for the results.
   virtual void integral(int n, double* wt, Func<double> **fns, Geom<double> *e, double* result)
   {
-    for (int i = 0; i < n; i++)
-      result[0] = wt[i] * fns[0]->val[i] * e->nx[i];
+    for(int j = 0; j < this->number_of_integrals; j++)
+      for (int i = 0; i < n; i++)
+        result[j] += wt[i] * fns[0]->val[i] * e->nx[i];
   };
 
   /// The integration order calculation.
   virtual void order(Func<Hermes::Ord> **fns, Hermes::Ord* result)
   {
-    result[0] = fns[0]->val[0];
+    for(int j = 0; j < this->number_of_integrals; j++)
+      result[j] = fns[0]->val[0];
   }
 };
--- inner-functionality/2d/integrals/integral-calculator/main.cpp
+++ inner-functionality/2d/integrals/integral-calculator/main.cpp
@@ -12,13 +12,24 @@ int main(int argc, char* argv[])
   // Define exact solution.
   MeshFunctionSharedPtr<double> exact_sln(new CustomExactSolution(mesh));
 
-  CustomVolumeIntegralCalculator volume_integral(Hermes::vector<MeshFunctionSharedPtr<double> >(exact_sln, exact_sln), 3);
-  CustomSurfaceIntegralCalculator surface_integral(Hermes::vector<MeshFunctionSharedPtr<double> >(exact_sln, exact_sln), 3);
+#ifdef _SHOW_OUTPUT
+  ScalarView s;
+  MeshView m;
+  s.show(exact_sln);
+  m.show(mesh);
+  View::wait();
+#endif
 
-  double volume = volume_integral.calculate("Copper")[0];
-  double surface = surface_integral.calculate("Outer")[0];
+  int random_number = std::max(2, std::rand());
 
-  bool success = Testing::test_value(volume, 1.01039, "volumetric", 1e-3) && Testing::test_value(surface, 1.07005, "surface", 1e-3);
+  CustomVolumeIntegralCalculator volume_integral(Hermes::vector<MeshFunctionSharedPtr<double> >(exact_sln, exact_sln), random_number);
+  CustomSurfaceIntegralCalculator surface_integral(Hermes::vector<MeshFunctionSharedPtr<double> >(exact_sln, exact_sln), random_number);
+
+  double volume = volume_integral.calculate("Copper")[random_number / 2];
+  double surface = surface_integral.calculate("Outer")[random_number / 2];
+
+  bool success = Testing::test_value(volume, 1.3927, "volumetric", 1e-3);
+  success = Testing::test_value(surface, 2., "surface", 1e-3) && success;
 
   if (success)
   {
