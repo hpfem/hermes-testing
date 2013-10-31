2de8c6fbf9e7adc56c431bf3165be83b1adee17d
Lukas Korous <lukas.korous@gmail.com>
2013-10-31 11:42:44 +0100

    Update CMake according to the library.

--- cmake/FindHERMES_COMMON.cmake
+++ cmake/FindHERMES_COMMON.cmake
@@ -1,9 +1,9 @@
 INCLUDE(FindPackageHandleStandardArgs)
 
 if(WIN64)
-  FIND_LIBRARY(HERMES_COMMON_LIBRARY NAMES hermes_common_64 hermes_common_64-debug PATHS ${HERMES_DIRECTORY} /usr/lib /usr/local/lib NO_DEFAULT_PATH)
+  FIND_LIBRARY(HERMES_COMMON_LIBRARY NAMES hermes_common_64 PATHS ${HERMES_DIRECTORY} /usr/lib /usr/local/lib NO_DEFAULT_PATH)
 else()
-  FIND_LIBRARY(HERMES_COMMON_LIBRARY NAMES hermes_common hermes_common-debug PATHS ${HERMES_DIRECTORY} /usr/lib /usr/local/lib NO_DEFAULT_PATH)
+  FIND_LIBRARY(HERMES_COMMON_LIBRARY NAMES hermes_common PATHS ${HERMES_DIRECTORY} /usr/lib /usr/local/lib NO_DEFAULT_PATH)
 endif()
   
 FIND_PATH(HERMES_COMMON_INCLUDE hermes_common.h ${HERMES_COMMON_INCLUDE_PATH} /usr/include/hermes_common /usr/local/include/hermes_common)
