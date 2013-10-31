b0c128c5295aa58842aafd31d1ea0831f526beba
Lukas Korous <lukas.korous@gmail.com>
2013-10-31 17:14:52 +0100

    Work on tests.

--- memory-leaks/2d/mesh/main.cpp
+++ memory-leaks/2d/mesh/main.cpp
@@ -4,9 +4,25 @@ int main(int argc, char* argv[])
 {
   // Load the mesh.
   MeshSharedPtr mesh(new Mesh);
+  MeshSharedPtr mesh2(new Mesh);
   MeshReaderH2DXML mloader;
   mloader.load("domain.xml", mesh);
   mloader.save("domain2.xml", mesh);
+  mloader.load("domain.xml", mesh2);
+  mesh->free();
+  mloader.load("domain.xml", mesh);
+
+#ifdef WITH_BSON
+  MeshReaderH2DBSON mloader_bson;
+
+  MeshSharedPtr mesh_bson(new Mesh);
+  MeshSharedPtr mesh_bson2(new Mesh);
+  mloader_bson.save("domain.bson", mesh);
+  mloader_bson.load("domain.bson", mesh_bson);
+  mloader_bson.save("domain2.bson", mesh_bson2);
+  mesh_bson->free();
+  mloader_bson.load("domain2.bson", mesh_bson);
+#endif
 
   return 0;
 }
--- memory-leaks/2d/nurbs/main.cpp
+++ memory-leaks/2d/nurbs/main.cpp
@@ -2,5 +2,32 @@
 
 int main(int argc, char* argv[])
 {
+  MeshSharedPtr mesh(new Mesh);
+  MeshReaderH2DXML mloader;
+  mloader.load("domain.xml", mesh);
+
+  RefMap rm;
+  MeshFunctionSharedPtr<double> sln(new Solution<double>(mesh));
+  
+  Element* e;
+  for(int ref = 0; ref < 5; ref++)
+  {
+    mesh->refine_all_elements();
+
+    MeshFunctionSharedPtr<double> zsln(new ZeroSolution<double>(mesh));
+
+    SpaceSharedPtr<double> space(new H1Space<double>(mesh, 3));
+    double* coeff = (double*)calloc(space->get_num_dofs(), sizeof(double));
+    Solution<double>::vector_to_solution(coeff, space, sln);
+
+    for_all_active_elements(e, mesh)
+    {
+      rm.set_active_element(e);
+      zsln->set_active_element(e);
+      sln->set_active_element(e);
+    }
+
+    ::free(coeff);
+  }
   return 0;
 }
