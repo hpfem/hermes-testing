6a2bbb408938d74a57f9a269535ad0a0eed2f4b7
Lukas Korous <lukas.korous@gmail.com>
2013-10-31 11:56:17 +0100

    Re-add specific names for double/release of hermes.

--- cmake/FindHERMES.cmake
+++ cmake/FindHERMES.cmake
@@ -2,9 +2,9 @@ INCLUDE(FindPackageHandleStandardArgs)
 
 if(${HERMES_VERSION} STREQUAL "HERMES2D")
   if(WIN64)
-    FIND_LIBRARY(HERMES_LIBRARY NAMES hermes2d_64 PATHS ${HERMES_DIRECTORY} /usr/lib /usr/local/lib NO_DEFAULT_PATH)
+    FIND_LIBRARY(HERMES_LIBRARY NAMES hermes2d_64 hermes2d_64-debug PATHS ${HERMES_DIRECTORY} /usr/lib /usr/local/lib NO_DEFAULT_PATH)
   else()
-    FIND_LIBRARY(HERMES_LIBRARY NAMES hermes2d PATHS ${HERMES_DIRECTORY} /usr/lib /usr/local/lib NO_DEFAULT_PATH)
+    FIND_LIBRARY(HERMES_LIBRARY NAMES hermes2d hermes2d-debug PATHS ${HERMES_DIRECTORY} /usr/lib /usr/local/lib NO_DEFAULT_PATH)
   endif()
   
   FIND_PATH(HERMES_INCLUDE hermes2d.h ${HERMES2D_INCLUDE_PATH} /usr/include/hermes2d /usr/local/include/hermes2d)
