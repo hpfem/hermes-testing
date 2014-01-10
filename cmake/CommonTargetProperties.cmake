d75dce35c75c08f375c61d9a96dcb9f0de43d6b2
Lukas Korous <lukas.korous@gmail.com>
2014-01-10 20:40:35 +0100

    Fixes.

--- cmake/CommonTargetProperties.cmake
+++ cmake/CommonTargetProperties.cmake
@@ -3,7 +3,12 @@ macro(SET_COMMON_TARGET_PROPERTIES TRGT HERMES_VERSION)
   find_package(HERMES REQUIRED)
 
 	target_link_libraries(${TRGT} ${HERMES_COMMON_LIBRARY})
-	target_link_libraries(${TRGT} ${HERMES_LIBRARY} ${MATIO_LIBRARY} ${BSON_LIBRARY})
+	target_link_libraries(${TRGT} ${HERMES_LIBRARY} ${MATIO_LIBRARY} ${BSON_LIBRARY} ${PJLIB_LIBRARY})
+  if(MSVC)
+    if(WITH_PJLIB)
+      TARGET_LINK_LIBRARIES(${TRGT} Ws2_32 wsock32)
+    endif(WITH_PJLIB)
+  endif(MSVC)
   target_link_libraries(${TRGT} ${TESTING_CORE_LIBRARY})
 
 	# Is empty if WITH_TRILINOS = NO
